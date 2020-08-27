72b8e2fc53a8d0d6eee116d86f5698ade6cf5b02
devaraujstein <andreearaujo98@gmail.com>
2020-08-27 13:40:31 -0300

    finalized catalog

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,31 +1,50 @@
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
     items: []
-}
+};
 
 const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-    switch(action.type){
-        case 'ADD_PRODUCT_TO_CART': {
-            const { product } = action.payload
-
-            return {
-                ...state,
-                items: [
-                    ...state.items,
-                    {
+    return produce(state, draft => {
+        switch(action.type){
+            case 'ADD_PRODUCT_TO_CART': {
+                const { product } = action.payload
+                
+                const productInCartIndex = draft.items.findIndex(item => 
+                    item.product.id === product.id,  
+                );
+                
+                if (productInCartIndex >= 0){
+                    draft.items[productInCartIndex].quantity += 1;
+                }else{
+                    draft.items.push({
                         product,
-                        quantity: 1,
-                    }
-                ]
-            };
+                        quantity: 1
+                    });
+                }
+                
+                break;
+                /*
+                return {
+                    ...state,
+                    items: [
+                        ...state.items,
+                        {
+                            product,
+                            quantity: 1,
+                        }
+                    ]
+                };
+                */
 
+            }
+            default: {
+                return draft;
+            }
         }
-        default: {
-            return state;
-        }
-    }
+    });
 }
 
 export default cart;
